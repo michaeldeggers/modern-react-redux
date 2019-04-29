fc7409f98cbcf429724e4a862e93ff092ad11100
michaeldeggers <michaeldeggers@gmail.com>
2019-04-29 16:14:45 -0400

    update spacing to match react standard (from 4 to 2)

--- components/src/index.js
+++ components/src/index.js
@@ -6,26 +6,26 @@ import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <CommentDetail
-                    author={faker.name.findName()}
-                    avatar={faker.image.avatar()}
-                    date={faker.date.recent().toDateString()}
-                    comment={faker.lorem.paragraph(3)}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author={faker.name.findName()}
-                    avatar={faker.image.avatar()}
-                    date={faker.date.recent().toDateString()}
-                    comment={faker.lorem.paragraph(3)}
-                />
-            </ApprovalCard>
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.findName()}
+          avatar={faker.image.avatar()}
+          date={faker.date.recent().toDateString()}
+          comment={faker.lorem.paragraph(3)}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.findName()}
+          avatar={faker.image.avatar()}
+          date={faker.date.recent().toDateString()}
+          comment={faker.lorem.paragraph(3)}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
