0d4bd1852d220073f082b57c6e1416165acc08c0
michaeldeggers <michaeldeggers@gmail.com>
2019-05-07 16:48:37 -0400

    add seasons module

---
+++ seasons/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.error(err)
+    );
+
+    return <div>Hi There</div>;
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
