b83d561edb32dc4249b80321aa1afb68c6900a90
michaeldeggers <michaeldeggers@gmail.com>
2019-06-06 19:47:43 -0400

    minor refactor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -9,19 +9,19 @@ class App extends React.Component {
     this.state = {
       lat: null // generally default eventual numbers to null
     }
-  }
 
-  // react says we have to define render
-  render() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
       },
       err => console.error(err)
     );
+  }
 
-    const latitude = this.state.latitude;
-    return <div style={{padding: `15px`}}>Latitude: {latitude}</div>;
+  // react says we have to define render
+  render() {
+    const lat = this.state.lat;
+    return <div style={{padding: `15px`}}>Latitude: {lat}</div>;
   }
 }
 
