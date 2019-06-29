8c1100770024c4bf5b689173c48853b668370089
michaeldeggers <michaeldeggers@gmail.com>
2019-06-29 13:37:41 -0400

    update to controlled component

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  // event automatically passed when using this callback
-  onInputChange(event) {
-    let text = event.target.value;
-    console.log(text);
-  }
+  state = { term: '' };
 
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input type="text" onChange={this.onInputChange}/>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}/>
           </div>
         </form>
       </div>
