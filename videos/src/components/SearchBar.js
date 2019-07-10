19237e1da1228be1fc42e7b35759d0ae046ece55
michaeldeggers <michaeldeggers@gmail.com>
2019-07-10 13:44:15 -0400

    - add basic form - make input controlled - add semantic ui

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  handleInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  handleFormSubmit = event => {
+    console.log(event);
+  };
+
   render () {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.handleFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" onChange={this.handleInputChange} value={this.state.term} />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
