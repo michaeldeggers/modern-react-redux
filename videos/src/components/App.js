c16e56ebff838a43a9570ef1f86fd1b53f5e0582
Michael Eggers <michael.eggers@syngenta.com>
2019-12-16 13:11:58 -0500

    - update with default search, appropriate attributes

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,26 +7,41 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.handleSearchSubmit('reactjs').then(() => console.log('videos loaded'))
+  }
+
   handleSearchSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
         q: term,
-      }
+      },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   handleVideoSelect = (selectedVideo) => {
-    this.setState({ selectedVideo })
+    this.setState({ selectedVideo });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar handleSearchSubmit={this.handleSearchSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} handleVideoSelect={this.handleVideoSelect} />
+        <SearchBar handleSearchSubmit={this.handleSearchSubmit}/>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo}/>
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} handleVideoSelect={this.handleVideoSelect}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
